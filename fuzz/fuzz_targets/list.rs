6347db6a8e5a292befe9da9c565bd3ad70a165ad
Michael Sproul <michael@sigmaprime.io>
2024-06-07 14:36:46 +1000

    Type check fuzzers on CI and fix another bug (#41)

--- fuzz/fuzz_targets/list.rs
+++ fuzz/fuzz_targets/list.rs
@@ -2,20 +2,28 @@
 
 use libfuzzer_sys::fuzz_target;
 use milhouse::List;
-use typenum::{Unsigned, U16, U32};
+use typenum::U16;
 
 fuzz_target!(|data: &[u8]| {
     let length = data.len();
-    if length > 256 {return}
+    if length > 256 {
+        return;
+    }
 
     // Create list using .push()
     let mut list1 = List::<u8, U16>::empty();
     for i in 0..length {
-        list1.push(data[i]);
+        if list1.push(data[i]).is_err() {
+            assert!(i >= 16);
+        }
     }
-    
-    list1.apply_updates();
+
+    list1.apply_updates().unwrap();
 
     // Create list using iterator
-    let list2 = List::<u8, U16>::try_from_iter(data.iter().copied());
+    let Ok(list2) = List::<u8, U16>::try_from_iter(data.iter().copied()) else {
+        return;
+    };
+
+    assert_eq!(list1, list2);
 });
